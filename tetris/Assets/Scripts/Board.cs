ff72a8d1c00ae5c783e3bc833ff811c9d8a12b4e
nzhul <dobromirivanov1@gmail.com>
2017-10-26 21:53:32 +0300

    update

--- tetris/Assets/Scripts/Board.cs
+++ tetris/Assets/Scripts/Board.cs
@@ -2,8 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Board : MonoBehaviour {
+public class Board : MonoBehaviour
+{
+    public Transform _emptySprite;
+    public int _height = 30;
+    public int _width = 10;
+    public int _header = 8;
 
+    Transform[,] _grid;
 
+    private void Awake()
+    {
+        _grid = new Transform[_width, _height];
+    }
 
+    void Start()
+    {
+        DrawEmptyCells();
+    }
+
+    void DrawEmptyCells()
+    {
+        if (_emptySprite != null)
+        {
+            for (int y = 0; y < _height - _header; y++)
+            {
+                for (int x = 0; x < _width; x++)
+                {
+                    Transform clone;
+                    clone = Instantiate(_emptySprite, new Vector3(x, y, 0), Quaternion.identity) as Transform;
+                    clone.name = "Board Space ( x = " + x.ToString() + " , y = " + y.ToString() + " )";
+                    clone.transform.parent = transform;
+
+                }
+            }
+        }
+        else
+        {
+            Debug.Log("WARNING! Please assign the emptySprite object!");
+        }
+    }
 }
