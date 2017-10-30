1c5f431b1eb9069f47aa2adb1ef414caad4e16a6
nzhul <dobromirivanov1@gmail.com>
2017-10-31 00:08:21 +0200

    up

---
+++ tetris/Assets/Scripts/Shape.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shape : MonoBehaviour {
+
+
+    public bool _canRotate = true;
+
+    void Move(Vector3 moveDirection)
+    {
+        transform.position += moveDirection;
+    }
+
+    public void MoveLeft()
+    {
+        Move(Vector3.left);
+    }
+
+    public void MoveRight()
+    {
+        Move(Vector3.right);
+    }
+
+    public void MoveDown()
+    {
+        Move(Vector3.down);
+    }
+
+    public void MoveUp()
+    {
+        Move(Vector3.up);
+    }
+
+    public void RoateRight()
+    {
+        if (_canRotate)
+        {
+            transform.Rotate(0, 0, -90);
+        }
+    }
+
+    public void RotateLeft()
+    {
+        if (_canRotate)
+        {
+            transform.Rotate(0, 0, 90);
+        }
+    }
+
+    private void Start()
+    {
+        
+    }
+}
